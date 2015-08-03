8bc3ff8b530efb060d7e8474f643dc736e2a66ac
Dahlia <fishball7557@gmail.com>
2015-08-03 21:55:42 +0800

    懲處記錄 CRUD

---
+++ app/controllers/admin/punishments_controller.rb
@@ -0,0 +1,63 @@
+class Admin::PunishmentsController < Admin::BaseController
+  before_action :punishment
+  before_action{ add_crumb("個人檔案列表", admin_profiles_path) }
+  before_action{ add_crumb("#{@profile.name}的個人檔案", admin_profile_path(@profile)) }
+  before_action(except: [:index]){ add_crumb("#{@profile.name}的懲處記錄列表", admin_profile_punishments_path(@profile)) }
+
+  def index
+    @punishments = @profile.punishments.all.page(params[:page]).per(10)
+    @admin_page_title = "#{@profile.name}的懲處記錄列表"
+    add_crumb @admin_page_title, "#"
+  end
+
+  def new
+    @admin_page_title = "新增#{@profile.name}的懲處記錄"
+    add_crumb @admin_page_title, "#"
+  end
+
+  def edit
+    @admin_page_title = "編輯#{@profile.name}的懲處記錄"
+    add_crumb @admin_page_title, "#"
+  end
+
+  def create
+    if punishment.save
+        respond_to do |f|
+          f.html { redirect_to admin_profile_punishments_path(@profile), flash: { success: "#{@profile.name}的懲處記錄 - 已新增" } }
+          f.js { render }
+        end
+    else
+      respond_to do |f|
+        f.html { render :new, flash: { error: punishment.errors.full_messages } }
+        f.js { render }
+      end
+    end
+  end
+
+  def update
+    if punishment.update_attributes(punishment_params)
+      redirect_to admin_profile_punishments_path(@profile), flash: { success: "#{@profile.name}的懲處記錄 - 已修改" }
+    else
+      render :edit, flash: { error: punishment.errors.full_messages }
+    end
+  end
+
+  def destroy
+    if punishment.destroy
+      redirect_to admin_profile_punishments_path(@profile), flash: { success: "#{@profile.name}的懲處記錄 - 已刪除" }
+    else
+      redirect_to :back, flash: { error: punishment.errors.full_messages }
+    end
+  end
+
+  private
+
+  def punishment
+    @profile = Admin::Profile.find params[:profile_id]
+    @punishment ||= params[:id] ? @profile.punishments.find(params[:id]) : @profile.punishments.new(punishment_params)
+  end
+
+  def punishment_params
+    params.fetch(:admin_punishment, {}).permit(:profile_id, :decision_unit, :unit, :title, :claimant, :no, :decision_no, :punish_type, :relevant_date, :decision_result, :decision_source, :reason, :is_anonymous, :anonymous_source, :anonymous, :origin_desc, :proposer, :plaintiff, :defendant, :reply, :reply_source, :punish, :content, :summary, :memo)
+  end
+end
---
+++ spec/requests/admin/punishments_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Admin::PunishmentsController do
+  let!(:profile){ FactoryGirl.create :profile }
+
+  before{ signin_user }
+
+  describe "already had a punishment" do
+    let!(:punishment){ FactoryGirl.create :punishment, profile: profile }
+
+    it "GET /admin/profiles/profile.id/punishments" do
+      get "/admin/profiles/#{profile.id}/punishments"
+      expect(response).to be_success
+    end
+
+    it "GET /admin/profiles/profile.id/punishments/new" do
+      get "/admin/profiles/#{profile.id}/punishments/new"
+      expect(response).to be_success
+    end
+
+    it "GET /admin/profiles/profile.id/punishments/123/edit" do
+      get "/admin/profiles/#{profile.id}/punishments/#{punishment.id}/edit"
+      expect(response).to be_success
+    end
+
+    it "PUT /admin/profiles/profile.id/punishments/123" do
+      expect{
+        put "/admin/profiles/#{profile.id}/punishments/#{punishment.id}", admin_punishment: { punish_type: "haha" }
+      }.to change{ punishment.reload.punish_type }.to("haha")
+      expect(response).to be_redirect
+    end
+
+    it "DELETE /admin/profiles/profile.id/punishments/123" do
+      delete "/admin/profiles/#{profile.id}/punishments/#{punishment.id}"
+      expect(Punishment.count).to be_zero
+    end
+  end
+
+  it "POST /admin/profiles/profile.id/punishments" do
+    expect{
+      post "/admin/profiles/#{profile.id}/punishments", admin_punishment: FactoryGirl.attributes_for(:punishment)
+    }.to change{ Punishment.count }.by(1)
+    expect(response).to be_redirect
+  end
+end
