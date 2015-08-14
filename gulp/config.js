48a3ee7e184e46432fa92eb997b652832e3ee18c
Jude Wang <jude@jude.asia>
2015-08-14 21:05:22 +0800

    setting up gulp scaffold

---
+++ gulp/config.js
@@ -0,0 +1,47 @@
+var manifestAssets = "./app/assets";
+var publicAssets   = "./public/assets";
+var sourceFiles    = "./gulp/assets";
+var bowerFiles     = "./vendor/assets/bower_components";
+
+module.exports = {
+  publicAssets: publicAssets,
+  browserSync: {
+    proxy: 'localhost:3000',
+    files: ['./app/views/**']
+  },
+  sass: {
+    src: sourceFiles + "/stylesheets/**/*.{sass,scss}",
+    dest: publicAssets + "/stylesheets",
+    settings: {
+      indentedSyntax: true, // Enable .sass syntax!
+      // sourceMap: true,
+      // includePaths: [bowerFiles],
+      imagePath: '/assets/images' // Used by the image-url helper
+    }
+  },
+  images: {
+    src: sourceFiles + "/images/**",
+    dest: publicAssets + "/images"
+  },
+  browserify: {
+    bundleConfigs: [{
+      entries: sourceFiles + '/javascripts/global.coffee',
+      dest: publicAssets + '/javascripts',
+      outputName: 'global.js',
+      extensions: ['.js','.coffee']
+    }]
+  },
+  wiredep: {
+    sassSrc: sourceFiles + '/stylesheets/_requirements.sass',
+    sassDest: sourceFiles + '/stylesheets',
+    options: {
+      // json: require('./bower.json'),
+      // directory: bowerFiles,
+      ignorePath: /^(\.\.\/)+/
+    }
+  },
+  svgSprite: {
+    src: sourceFiles + '/icons/**',
+    dest: publicAssets + '/images'
+  }
+};
