3b92e7b65f31337bdc31fcffadee3e1b8c88647b
Jude Wang <jude@jude.asia>
2015-08-18 18:11:58 +0800

    add svgs to build tasks

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,7 +2,7 @@ var gulp         = require('gulp');
 var gulpSequence = require('gulp-sequence')
 
 gulp.task('build', function(cb) {
-  var tasks = ['clean',['images', 'svg-sprite'], ['sass', 'browserify']];
+  var tasks = ['clean',['images', 'svgs', 'svg-sprite'], ['sass', 'browserify']];
   if(process.env.RAILS_ENV === 'production') tasks.push('rev');
   tasks.push(cb);
   gulpSequence.apply(this, tasks);
