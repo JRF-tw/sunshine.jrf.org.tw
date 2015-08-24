54192c604ffb3795c05aea3041730a97a76d4554
Jude Wang <jude@jude.asia>
2015-08-24 16:33:31 +0800

    added gulp responsive task

---
+++ gulp/tasks/responsive.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp');
+var changed     = require('gulp-changed');
+var responsive  = require('gulp-responsive');
+var browserSync = require('browser-sync');
+var config      = require('../config').responsive;
+
+gulp.task('responsive', function () {
+  return gulp.src(config.src)
+    .pipe(changed(config.dest))
+    .pipe(responsive(buildSet(config.sets)))
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({stream:true}));    
+});
+
+function widthSet (widths) {
+  var   i = widths.length,
+      set = [];
+  while (i--) {
+    set.push({
+      width: widths[i],
+      rename: {suffix: '-' + widths[i]}
+    });
+  }
+  return set;
+}
+
+function buildSet (sets) {
+  var result = {},
+           i = sets.length;
+  while (i--) {
+    result[sets[i].name] = widthSet(sets[i].widths);
+  }
+  return result;
+}
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,11 +7,12 @@ var gulp   = require('gulp');
 var config = require('../config');
 var watch  = require('gulp-watch');
 
-gulp.task('watch', ['watchify','browserSync'], function(callback) {
+gulp.task('watch', ['watchify','browserSync'], function() {
   watch(config.sass.src, function() { gulp.start('sass'); });
   watch(config.images.src, function() { gulp.start('images'); });
   watch('./bower.json', function() { gulp.start('wiredep'); });
   watch(config.svgs.src, function() { gulp.start('svgs'); });
   watch(config.svgSprite.src, function() { gulp.start('svg-sprite'); });
+  watch(config.responsive.src, function () { gulp.start('responsive');});
   // Watchify will watch and recompile our JS, so no need to gulp.watch it
 });
