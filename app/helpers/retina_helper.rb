80cb1ca82bf4ac54b1d752206576548c2f819215
Jude Wang <jude@jude.asia>
2015-08-26 17:43:55 +0800

    update comment

--- app/helpers/retina_helper.rb
+++ app/helpers/retina_helper.rb
@@ -18,6 +18,9 @@ module RetinaHelper
       'data-sizes'  => 'auto',
       'data-srcset' => _path.gsub(/\./) { |match| '-{width}.' },
       'data-widths' => sizes.to_s # 縮放後的尺寸列表以陣列來帶入
+
+    # 所有的正規表示式都只是在把寬度塞進副檔名前
+    # eg. filename.jpg -> filename-300.jpg
   end
 
 end
