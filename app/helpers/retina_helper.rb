3dbc3b0467c83997e679bf28b454d9cacfa9bd0e
Jude Wang <jude@jude.asia>
2015-08-27 23:34:27 +0800

    fix typo

--- app/helpers/retina_helper.rb
+++ app/helpers/retina_helper.rb
@@ -2,7 +2,7 @@ module RetinaHelper
 
   def inline_svg(icon)
     content_tag :svg, class: "#{icon} icon" do
-      tag :use, "xlink:href" => "##{icon}"
+      tag :use, "xlink:href" => "#icon-#{icon}"
     end
   end
   
