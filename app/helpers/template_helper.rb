e712ff6fdcbedc0a15af0a7efafaab19258efc91
Jude Wang <jude@jude.asia>
2015-09-01 00:05:14 +0800

    change helper to partial

--- app/helpers/template_helper.rb
+++
@@ -1,8 +0,0 @@
-module TemplateHelper
-  def feature_card(heading, &block)
-    content_tag :section, class: 'feature-card' do
-      concat content_tag :h3, heading, class: 'feature-card__heading'
-      concat capture &block
-    end
-  end
-end
