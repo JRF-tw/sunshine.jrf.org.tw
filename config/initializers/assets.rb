d5197992e21623e7ac2f7c4ca0e1ac879ae9123c
ohm kao <ohm.kao@gmail.com>
2015-09-11 01:43:15 +0800

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( admin.css admin.js )
+# Rails.application.config.assets.precompile += %w( admin.css admin.js policies.js *.svg *.eot *.woff *.ttf *.map *.png *.jpg *.json)
+
+Rails.application.config.assets.precompile += [/^[a-z0-9]\w+.(css|js|woff|eot|svg|ttf|map|png|jpg|gif|json)$/]
