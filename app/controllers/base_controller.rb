d6fd12e5a0ab944b37802feebba3cd83af588b2c
Dahlia <fishball7557@gmail.com>
2015-09-15 17:22:34 +0800

    首頁套版

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,6 +2,9 @@ class BaseController < ApplicationController
   
   def index
     set_meta(title: "首頁")
+    @suits = Suit.last(3)
+    @judges = Profile.judges.shuffle.last(12)
+    @prosecutors = Profile.prosecutors.shuffle.last(12)
   end
 
   def about
