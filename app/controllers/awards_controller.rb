981461faed7ddaa6b962ab0acf4b57c1d9f21110
Dahlia <fishball7557@gmail.com>
2015-09-18 18:33:38 +0800

    套版：獎勵記錄頁

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,6 +1,10 @@
 class AwardsController < BaseController
   def index
     @profile = Profile.find(params[:profile_id])
+    @awards = @profile.awards.newest
+    @newest_award = @awards.last
+    @newest_punishments = @profile.punishments.had_reason.newest.first(3)
+    @newest_reviews = @profile.reviews.had_title.newest.first(3)
     set_meta(title: "獎勵記錄")
   end
 end
