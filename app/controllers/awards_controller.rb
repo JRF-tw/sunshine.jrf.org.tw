b3791aa5576aeef770828219820e4bb3346156ff
Dahlia <fishball7557@gmail.com>
2015-09-18 22:21:34 +0800

    套版：懲處記錄頁

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -3,7 +3,7 @@ class AwardsController < BaseController
     @profile = Profile.find(params[:profile_id])
     @awards = @profile.awards.newest
     @newest_award = @awards.last
-    @newest_punishments = @profile.punishments.had_reason.newest.first(3)
+    @newest_punishments = @profile.punishments.newest.first(3)
     @newest_reviews = @profile.reviews.had_title.newest.first(3)
     set_meta(title: "獎勵記錄")
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,4 +51,58 @@ module ApplicationHelper
       content
     end
   end
+
+  def render_punishment_cell(punishment)
+    hash = Hash.new
+    if punishment.decision_unit == "公懲會"
+      hash = {
+        "議決日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": punishment.decision_unit.present? ? punishment.decision_unit : nil,
+        "移送機關": punishment.claimant.present? ? punishment.claimant : nil,
+        "決議結果": punishment.decision_result.present? ? punishment.decision_result : nil
+      }
+    elsif punishment.decision_unit == "檢評會"
+      hash = {
+        "決議日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": punishment.decision_unit.present? ? punishment.decision_unit : nil,
+        "請 求 人": punishment.claimant.present? ? punishment.claimant : nil
+      }
+    elsif punishment.decision_unit == ("監察院(新)" || "監察院")
+      hash = {
+        "議決日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": "監察院",
+        "決議案號": punishment.decision_no.present? ? punishment.decision_no : nil,
+        "決議種類": punishment.punish_type.present? ? punishment.punish_type : nil
+      }
+    elsif punishment.decision_unit == "監察院(舊)"
+      hash = {
+        "公告日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": "監察院",
+        "發文字號": punishment.decision_no.present? ? punishment.decision_no : nil,
+        "決議種類": punishment.punish_type.present? ? punishment.punish_type : nil
+      }
+    elsif punishment.decision_unit == "職務法庭"
+      hash = {
+        "裁判日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": punishment.decision_unit.present? ? punishment.decision_unit : nil,
+        "移送機關": punishment.claimant.present? ? punishment.claimant : nil
+      }
+    elsif punishment.decision_unit.to_s == "法評會"
+      hash = {
+        "決議日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": punishment.decision_unit.present? ? punishment.decision_unit : nil,
+        "請 求 人": punishment.claimant.present? ? punishment.claimant : nil
+      }
+    elsif punishment.decision_unit == "司法院"
+      hash = {
+        "公告日期": punishment.relevant_date.present? ? punishment.relevant_date : nil,
+        "決定機關": punishment.decision_unit.present? ? punishment.decision_unit : nil,
+        "懲處機關": punishment.claimant.present? ? punishment.claimant : nil,
+        "會議名稱": punishment.decision_no.present? ? punishment.decision_no : nil,
+        "懲處種類": punishment.punish_type.present? ? punishment.punish_type : nil
+      }
+    end
+    hash.delete_if { |k, v| v == nil }
+    hash
+  end
 end
