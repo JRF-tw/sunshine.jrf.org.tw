1c0831c695fc392a372466f7dfeb0c13d0be7a68
Dahlia <fishball7557@gmail.com>
2015-09-24 20:18:26 +0800

    wording fix

--- app/controllers/admin/banners_controller.rb
+++ app/controllers/admin/banners_controller.rb
@@ -1,33 +1,33 @@
 class Admin::BannersController < Admin::BaseController
   before_action :banner
-  before_action(except: [:index]){ add_crumb("首頁 banner 列表", admin_banners_path) }
+  before_action(except: [:index]){ add_crumb("首頁橫幅列表", admin_banners_path) }
 
   def index
     @banners = Banner.all.order_by_weight.page(params[:page]).per(10)
-    @admin_page_title = "首頁 banner 列表"
+    @admin_page_title = "首頁橫幅列表"
     add_crumb @admin_page_title, "#"
   end
 
   def new
-    @admin_page_title = "新增首頁 banner"
+    @admin_page_title = "新增首頁橫幅"
     add_crumb @admin_page_title, "#"
   end
 
   def edit
-    @admin_page_title = "編輯首頁 banner"
+    @admin_page_title = "編輯首頁橫幅"
     add_crumb @admin_page_title, "#"
   end
 
   def create
     if banner.save
         respond_to do |f|
-          f.html { redirect_to admin_banners_path, flash: { success: "首頁 banner 已新增" } }
+          f.html { redirect_to admin_banners_path, flash: { success: "首頁橫幅已新增" } }
           f.js { render }
         end
     else
       respond_to do |f|
         f.html {
-          @admin_page_title = "新增首頁 banner"
+          @admin_page_title = "新增首頁橫幅"
           add_crumb @admin_page_title, "#"
           flash[:error] = banner.errors.full_messages
           render :new
@@ -39,9 +39,9 @@ class Admin::BannersController < Admin::BaseController
 
   def update
     if banner.update_attributes(banner_params)
-      redirect_to admin_banners_path, flash: { success: "首頁 banner 已修改" }
+      redirect_to admin_banners_path, flash: { success: "首頁橫幅已修改" }
     else
-      @admin_page_title = "編輯首頁 banner"
+      @admin_page_title = "編輯首頁橫幅"
       add_crumb @admin_page_title, "#"
       flash[:error] = banner.errors.full_messages
       render :edit
@@ -50,7 +50,7 @@ class Admin::BannersController < Admin::BaseController
 
   def destroy
     if banner.destroy
-      redirect_to admin_banners_path, flash: { success: "首頁 banner 已刪除" }
+      redirect_to admin_banners_path, flash: { success: "首頁橫幅已刪除" }
     else
       flash[:error] = banner.errors.full_messages
       redirect_to :back
