b736f1998de6089b37191f75ee89695995db7b70
billy3321 <billy3321@gmail.com>
2015-11-19 15:54:40 +0800

    now can edit current_court

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -70,6 +70,6 @@ class Admin::ProfilesController < Admin::BaseController
   end
 
   def profile_params
-    params.fetch(:admin_profile, {}).permit(:name, :current, :avatar, :remove_avatar, :gender, :birth_year, :stage, :appointment, :memo, :gender_source, :birth_year_source, :stage_source, :appointment_source, :is_active, :is_hidden)
+    params.fetch(:admin_profile, {}).permit(:name, :current, :current_court, :avatar, :remove_avatar, :gender, :birth_year, :stage, :appointment, :memo, :gender_source, :birth_year_source, :stage_source, :appointment_source, :is_active, :is_hidden)
   end
 end
