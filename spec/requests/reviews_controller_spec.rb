39256e7d04f432e4d51518ab8a69b85d1994b050
billy3321 <billy3321@gmail.com>
2015-11-26 15:22:44 +0800

    remove review page test.

--- spec/requests/reviews_controller_spec.rb
+++ spec/requests/reviews_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe ReviewsController, :type => :request do
   let!(:profile){ FactoryGirl.create :profile }
   let!(:review){ FactoryGirl.create :review, profile: profile }
   
-  it "GET /profiles/profile.id/reviews" do
-    get "/profiles/#{profile.id}/reviews"
-    expect(response).to be_success
-  end
+  # it "GET /profiles/profile.id/reviews" do
+  #   get "/profiles/#{profile.id}/reviews"
+  #   expect(response).to be_success
+  # end
 end
