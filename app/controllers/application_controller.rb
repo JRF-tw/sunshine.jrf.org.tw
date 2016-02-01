b8e1108b7747cfd70b9ff5112512ac54df59fd18
billy3321 <billy3321@gmail.com>
2016-02-01 13:43:13 +0800

    fix path name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || admin_path
+    stored_location_for(resource) || admin_root_path
   end
 
   private
