9151693ea64927ea72cef493e772d39690eb86d2
phrase5036 <phrase5036@gmail.com>
2016-04-08 12:51:55 +0800

    implement BaseContext

---
+++ app/lib/errors/exception.rb
@@ -0,0 +1,20 @@
+class Errors::Exception < ::Exception
+  attr_accessor :key, :info
+
+  def initialize(key, info = {})
+    self.key = key
+    self.info = info
+  end
+
+  def message
+    info.delete(:message) || (Code.desc(key) + (Rails.env.production? ? "" : info.inspect))
+  end
+
+  def to_hash
+    {
+      message:  message,
+      info:     info,
+      key:      key
+    }
+  end
+end
