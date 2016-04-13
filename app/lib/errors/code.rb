668fe82945584d1918ebb1ddafba3769472445e7
Aron <h0935674938@hotmail.com>
2016-04-13 09:57:22 +0800

    fix conflict

--- app/lib/errors/code.rb
+++ app/lib/errors/code.rb
@@ -1,5 +1,15 @@
 class Errors::Code
-  STATUS = {}.freeze
+
+  STATUS = {
+    error_code_not_defined: 400,
+    params_required: 400,
+    data_not_found: 404,
+    value_blank: 400,
+    validates_fail: 400,
+    data_create_fail: 400,
+    data_update_fail: 400,
+    data_delete_fail: 400
+  }.freeze
 
   class << self
     def exists?(key)
