943b54ab087126af719542aecf42792b921c300d
Aron <h0935674938@hotmail.com>
2016-04-21 16:59:38 +0800

    remove attributes assign test

--- spec/contexts/lawyer_create_context_spec.rb
+++ spec/contexts/lawyer_create_context_spec.rb
@@ -5,18 +5,12 @@ describe LawyerCreateContext do
   context "success" do 
     let(:params) { attributes_for(:lawyer) }
     subject { described_class.new(params) }
-    it { 
-      expect { subject.perform }.to change { Lawyer.count }.by(1)
-      expect { assigns(:lawyers, Lawyer.new) }
-    }
+    it { expect { subject.perform }.to change { Lawyer.count }.by(1) }
   end
 
   context "name can't be empty" do 
     let(:empty_name) { attributes_for(:empty_name_for_lawyer) }
     subject { described_class.new(empty_name) }
-    it { 
-      expect { subject.perform }.not_to change{ Lawyer.count } 
-      expect { assigns(:lawyers) }
-    }
+    it { expect { subject.perform }.not_to change{ Lawyer.count } }
   end
 end 
