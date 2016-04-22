acc12653046d9c747e1d78e18bb79e54f7146f88
phrase5036 <phrase5036@gmail.com>
2016-04-22 14:05:46 +0800

    deploy linked_dirs without bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :scm, :git
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
