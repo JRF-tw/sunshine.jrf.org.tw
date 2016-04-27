febd389705dbc7ee548d1d607f9654ae5e107013
Aron <h0935674938@hotmail.com>
2016-04-27 16:46:30 +0800

    story rspec for model ,controller

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe Story do
       it { expect(story).not_to be_new_record }
     end
   end
+
+  context "story_detail" do
+    let(:stroy1) { FactoryGirl.create :story ,year: 100, word_type: "耶", number: 100}
+    it { expect(stroy1.story_detail).to eq("100-耶-100") }
+  end
 end
