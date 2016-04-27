409ecccabfe6725c0ad0100955c167c7d11151d2
Aron <h0935674938@hotmail.com>
2016-04-27 18:25:30 +0800

    add rspec

--- spec/requests/admin/stories_controller_spec.rb
+++ spec/requests/admin/stories_controller_spec.rb
@@ -9,10 +9,15 @@ RSpec.describe Admin::StoriesController do
     context "search the story_type of stories" do
       before { get "/admin/stories", q: { story_type: "邢事" } }
       it { expect(response.body).to match(story.story_type) }
-    end 
+    end
+
+    context "search the main_judge of stories" do
+      before { get "/admin/stories", q: { main_judge_id_eq: story.main_judge.id} }
+      it { expect(response.body).to match(story.word_type) }
+    end
 
     context "render success" do
-      before { get "/admin/courts" }
+      before { get "/admin/stories" }
       it { expect(response).to be_success }
     end
   end  
