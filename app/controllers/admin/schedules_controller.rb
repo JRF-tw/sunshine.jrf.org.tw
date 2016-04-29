f3cad51f909f67a0601230a72869161ceb32be87
Aron <h0935674938@hotmail.com>
2016-04-29 14:04:26 +0800

    stories view and schedules controller

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -12,12 +12,19 @@
 #
 
 class Admin::SchedulesController < Admin::BaseController
+   before_action :find_story_by_ransack_search, :only => [:index]
 
   def index
     @search = Schedule.all.newest.ransack(params[:q])
     @schedules = @search.result.includes(:court, :story).page(params[:page]).per(20)
-    @admin_page_title = "庭期表列表"
+    @admin_page_title = @story ? "案件 #{@story.identity} 的庭期表" : "庭期表列表"
     add_crumb @admin_page_title, "#"
   end
+
+  private
+  
+  def find_story_by_ransack_search
+    return @story = Story.find(params[:q][:story_id_eq]) if params[:q].try { |q| q.include?(:story_id_eq) }
+  end  
 
 end
