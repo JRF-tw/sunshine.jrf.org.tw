cc4d011ad0b9460c511f8919afa0306c7633c017
Aron <h0935674938@hotmail.com>
2016-05-02 15:51:01 +0800

    after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20160502031411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
   enable_extension "postgis"
+  enable_extension "hstore"
 
   create_table "articles", force: :cascade do |t|
     t.integer  "profile_id"
@@ -465,6 +465,7 @@ ActiveRecord::Schema.define(version: 20160502031411) do
     t.text     "lawyer_names"
     t.text     "judges_names"
     t.text     "prosecutor_names"
+    t.date     "adjudge_date"
     t.boolean  "is_adjudge",       default: false
     t.date     "adjudge_date"
   end
