5e51add9662b53ce67b1da4874890768742072de
phrase5036 <phrase5036@gmail.com>
2016-05-03 05:46:58 +0800

    判決書後台新增搜尋資料分析結果

--- app/models/verdict.rb
+++ app/models/verdict.rb
@@ -24,4 +24,26 @@ class Verdict < ActiveRecord::Base
 
   scope :newest, ->{ order("id DESC") }
 
+  class << self
+    def ransackable_scopes(auth_object = nil)
+      [ :unexist_defendant_names, :unexist_lawyer_names, :unexist_judges_names, :unexist_prosecutor_names ]
+    end
+
+    def unexist_defendant_names
+      where(defendant_names: nil)
+    end
+
+    def unexist_lawyer_names
+      where(lawyer_names: nil)
+    end
+
+    def unexist_judges_names
+      where(judges_names: nil)
+    end
+
+    def unexist_prosecutor_names
+      where(prosecutor_names: nil)
+    end
+  end
+
 end
