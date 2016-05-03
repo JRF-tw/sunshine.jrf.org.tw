d1ac55def1729964b3c24fedc18fc7f1437d3a32
phrase5036 <phrase5036@gmail.com>
2016-05-03 21:13:35 +0800

    fix spec error

--- spec/models/verdict_spec.rb
+++ spec/models/verdict_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Verdict do
   let(:verdict){ FactoryGirl.create :verdict }
 
   describe "FactoryGirl" do
-    cntext "normalize" do
+    context "normalize" do
       subject!{ verdict }
       it { expect(subject).not_to be_new_record }
     end
 
-    cntext "with_main_judge" do
+    context "with_main_judge" do
       let(:verdict){ FactoryGirl.create :verdict, :with_main_judge }
       subject!{ verdict }
       it { expect(subject).not_to be_new_record }
