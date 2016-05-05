1abd2350ead4ca8aa84ef124c0d0cd8ad0507505
Aron <h0935674938@hotmail.com>
2016-05-05 14:32:33 +0800

    rspec

--- spec/requests/admin/stories_controller_spec.rb
+++ spec/requests/admin/stories_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin::StoriesController do
   before{ signin_user }
-  let!(:story) { FactoryGirl.create :story, story_type: "邢事", year: 1990, word_type: "火箭", number: 100 }
+  let!(:story) { FactoryGirl.create :story, story_type: "邢事", year: 1990, word_type: "火箭", number: 100, is_adjudge: true }
 
   describe "#index" do
     context "search the story_type of stories" do
@@ -11,13 +11,18 @@ RSpec.describe Admin::StoriesController do
     end
 
     context "search the main_judge of stories" do
-      before { get "/admin/stories", q: { main_judge_id_eq: story.main_judge.id} }
+      before { get "/admin/stories", q: { main_judge_id_eq: story.main_judge.id } }
       it { expect(response.body).to match(story.word_type) }
     end
 
 
     context "search the adjudge_date of stories" do
       before { get "/admin/stories", q: { adjudge_date_eq: story.adjudge_date} }
+      it { expect(response.body).to match(story.word_type) }
+    end
+
+    context "search the is_adjudge of stories" do
+      before { get "/admin/stories", q: { is_adjudge_eq: true } }
       it { expect(response.body).to match(story.word_type) }
     end
 
