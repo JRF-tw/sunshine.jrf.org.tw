67020d03977a5975cc0fefd33a0f9b892ccd6d6a
phrase5036 <phrase5036@gmail.com>
2016-05-05 12:07:24 +0800

    fix up

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,11 @@ module JrfSunny
     # factory girl rails
     config.generators do |g|
       g.test_framework :rspec, fixture: true, views: false, fixture_replacement: :factory_girl
-      g.factory_girl dir: "spec/factories" 
+      g.factory_girl dir: "spec/factories"
     end
 
     config.to_prepare do
-      Devise::SessionsController.layout "admin" 
+      Devise::SessionsController.layout "admin"
     end
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -32,7 +32,7 @@ module JrfSunny
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Taipei'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
