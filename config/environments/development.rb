ad47beb1cad587c762ea9637525bd203f882c38d
Aron <h0935674938@hotmail.com>
2016-05-17 18:23:11 +0800

    寄信功能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
