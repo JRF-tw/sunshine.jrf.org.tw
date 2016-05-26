382319451f19632c02d23d9d3a7834d4c84c8dc4
phrase5036 <phrase5036@gmail.com>
2016-05-26 19:39:49 +0800

    add defendants/registrations controller + view

---
+++ app/controllers/defendants/registrations_controller.rb
@@ -0,0 +1,48 @@
+class Defendants::RegistrationsController < Devise::RegistrationsController
+  layout 'defendant'
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # POST /resource/check_sign_up_info
+  def check_sign_up_info
+    build_resource(sign_up_params)
+    render "defendants/registrations/sign_up_password"
+  end
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      set_flash_message :notice, :"signed_up_failed" if is_flashing_format?
+      redirect_to new_defendant_registration_path
+    end
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    defendants_root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    # TODO 輸入手機驗證碼頁面
+    defendants_root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:identify_number, :name]
+  end
+end
