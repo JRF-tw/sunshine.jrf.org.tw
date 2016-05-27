b2e95378847326912a9cbd697f45390ce473bfe6
Aron <h0935674938@hotmail.com>
2016-05-27 15:40:30 +0800

    code review fix

--- spec/requests/bystander/registrations_controller_spec.rb
+++ spec/requests/bystander/registrations_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Bystander::RegistrationsController, :type => :request do
   describe "#update" do
     before { signin_bystander }
     let!(:bystander) { current_bystander }
+    let!(:old_email) { bystander.email }
 
     context "update email" do
       before { put "/bystanders", bystander: { email: "h2312@gmail.com", current_password: "123123123"} }
@@ -16,17 +17,32 @@ RSpec.describe Bystander::RegistrationsController, :type => :request do
       it { expect(response.body).to match("e-mail 並未更改")}
     end
 
-    context "sign in with unconfirmed_email" do
+    context "sign in after updated" do
       before { put "/bystanders", bystander: { email: "h2312@gmail.com", current_password: "123123123"} }
       before { signout_bystander }
+      subject { post "/bystanders/sign_in", bystander: { email: bystander.email, password: "123123123" } }
+
+      it { expect{ subject }.to change { bystander.reload.current_sign_in_at } }
+    end
+
+    context "sign in new email after updated and confirm" do
+      before { put "/bystanders", bystander: { email: "h2312@gmail.com", current_password: "123123123"} }
+      before { signout_bystander }
+      before { get "/bystanders/confirmation", confirmation_token: bystander.reload.confirmation_token }
+
       subject { post "/bystanders/sign_in", bystander: { email: "h2312@gmail.com", password: "123123123" } }
-
-      it { expect{ subject }.to_not change {bystander.reload.last_sign_in_at} }
+      it { expect{ subject }.to change { bystander.reload.current_sign_in_at } }
     end
 
-    context "sign in with confirmed_email" do
-      it "not done yet"
+    context "sign in old email after updated and confirm" do
+      before { put "/bystanders", bystander: { email: "h2312@gmail.com", current_password: "123123123"} }
+      before { signout_bystander }
+      before { get "/bystanders/confirmation", confirmation_token: bystander.reload.confirmation_token }
+
+      subject { post "/bystanders/sign_in", bystander: { email: old_email, password: "123123123" } }
+      it { expect{ subject }.to_not change { bystander.reload.current_sign_in_at } }
     end
+
   end
 
 end
