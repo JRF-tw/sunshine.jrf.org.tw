2dfdfe090dae18bab70474b19ff55eb2fbfc099f
Aron <h0935674938@hotmail.com>
2016-05-30 16:47:50 +0800

    問題三 效能改善

--- app/controllers/admin/verdicts_controller.rb
+++ app/controllers/admin/verdicts_controller.rb
@@ -23,7 +23,7 @@ class Admin::VerdictsController < Admin::BaseController
 
   def index
     @search = Verdict.all.newest.ransack(params[:q])
-    @verdicts = @search.result.page(params[:page]).per(20)
+    @verdicts = @search.result.includes(:story).page(params[:page]).per(20)
     @admin_page_title = "判決書列表"
     add_crumb @admin_page_title, "#"
   end
