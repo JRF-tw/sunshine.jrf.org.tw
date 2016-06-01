a51f92b31033a72e99d48e1780d5134e67fbb8ac
Aron <h0935674938@hotmail.com>
2016-06-01 18:11:25 +0800

    律師 rspec

--- spec/support/request_client.rb
+++ spec/support/request_client.rb
@@ -30,6 +30,13 @@ module RequestClient
     @current_defendant = defendant if response.status == 302
   end
 
+  def signin_lawyer(lawyer = nil)
+    lawyer ||= FactoryGirl.create(:lawyer, :with_password_and_confirmed)
+    data =
+    post "/lawyers/sign_in", lawyer: { email: lawyer.email, password: lawyer.password }
+    @current_lawyer = lawyer if response.status == 302
+  end
+
   def current_user
     @current_user
   end
