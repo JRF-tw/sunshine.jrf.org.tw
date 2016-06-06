cc86cdaec6140f640328e6a6e5a052a75eea7e43
Aron <h0935674938@hotmail.com>
2016-06-06 16:03:51 +0800

    當事人身分證格式驗證 & 手機格式驗證 ＋ model 測試

--- spec/models/defendant_spec.rb
+++ spec/models/defendant_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Defendant, type: :model do
   let(:defendant){ FactoryGirl.create :defendant }
-
   it "FactoryGirl" do
     expect(defendant).not_to be_new_record
   end
@@ -13,5 +12,33 @@ RSpec.describe Defendant, type: :model do
       it { expect(defendant.reload.reset_password_token).to be_present }
       it { expect(defendant.reload.reset_password_sent_at).to be_present }
     end
+  end
+
+  describe "validate regexp_match" do
+    context "success" do
+      let(:defendant_re) { Defendant.new(name: "aron", identify_number: "S211111111", password: "11111111") }
+      it { expect(defendant_re.valid?).to eq(true) }
+    end
+
+    context "success with phone_number" do
+      let(:defendant_re) { Defendant.new(name: "aron", identify_number: "S211111111", password: "11111111", phone_number: "0911111111") }
+      it { expect(defendant_re.valid?).to eq(true) }
+    end
+
+    context "identify_number empty" do
+      let(:defendant_re) { Defendant.new(name: "aron", password: "11111111") }
+      it { expect(defendant_re.valid?).to eq(false) }
+    end
+
+    context "identify_number doesn't match" do
+      let(:defendant_re) { Defendant.new(name: "aron", identify_number: "S311111111") }
+      it { expect(defendant_re.valid?).to eq(false) }
+    end
+
+    context "phone_number doesn't match" do
+      let(:defendant_re) { Defendant.new(name: "aron", identify_number: "S111111111", phone_number: "0811294939") }
+      it { expect(defendant_re.valid?).to eq(false) }
+    end
+
   end
 end
