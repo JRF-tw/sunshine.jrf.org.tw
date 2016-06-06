bb77725adb29b001d8790ad0957934d01041fed9
phrase5036 <phrase5036@gmail.com>
2016-06-06 16:32:40 +0800

    關閉staging的通知

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -1,6 +1,7 @@
 class SlackService
   WEBHOOK = "https://hooks.slack.com/services/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".freeze
   DEFAULT_ICON_URL = "http://i.imgur.com/kwu9VJF.jpg".freeze
+  DISABLED_ENV = ["staging"]
 
   class << self
     def notify(message, channel: "#general", name: "slack-robot", icon_url: DEFAULT_ICON_URL, webhook: nil)
@@ -12,32 +13,32 @@ class SlackService
     end
 
     def notify_async(message, channel: "#general", name: "slack-robot", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
     end
 
     def notify_scrap_async(message, channel: "#notify-scrap-error", name: "Exception", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
       false
     end
 
     def notify_analysis_async(message, channel: "#notify-scrap-analysis", name: "slack-robot", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
     end
 
     def notify_scrap_daily_async(message, channel: "#notify-scrap-daily", name: "scrap-daily", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
     end
 
     def notify_court_alert_async(message, channel: "#notify-court", name: "scrap-court", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
     end
 
     def notify_branch_alert_async(message, channel: "#notify-branch", name: "scrap-branch", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
     end
 
     def notify_verdict_alert_async(message, channel: "#notify-verdict", name: "scrap-verdict", icon_url: DEFAULT_ICON_URL, webhook: nil)
-      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook)
+      delay.notify(message, channel: channel, name: name, icon_url: icon_url, webhook: webhook) unless DISABLED_ENV.include?(Rails.env)
     end
 
     def fake_defendant_reset_password_notify_async(message, channel: "#notify-developer", name: "notify", icon_url: DEFAULT_ICON_URL, webhook: nil)
@@ -49,5 +50,4 @@ class SlackService
       "<#{link}|#{message}>"
     end
   end
-
 end
