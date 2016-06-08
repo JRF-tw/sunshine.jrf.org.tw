a514f9ea21d2c32c017a565f9bc9140744e8388e
Aron <h0935674938@hotmail.com>
2016-06-08 11:00:31 +0800

    rspec fix

--- spec/contexts/admin/lawyer_create_context_spec.rb
+++ spec/contexts/admin/lawyer_create_context_spec.rb
@@ -2,31 +2,35 @@ require 'rails_helper'
 
 describe Admin::LawyerCreateContext do
 
-  describe "success" do
-    let(:params) { attributes_for(:lawyer) }
-    subject { described_class.new(params) }
-
+  describe "perform" do
     context "create success" do 
+      let(:params) { attributes_for(:lawyer) }
+      subject { described_class.new(params) }
+
       it { expect { subject.perform }.to change { Lawyer.count }.by(1) }
     end
 
     context "created should unconfirm" do
+      let(:params) { attributes_for(:lawyer) }
       before { subject.perform }
+      subject { described_class.new(params) }
+      
       it { expect(Lawyer.last.confirmed?).to be false }
     end
-  end
+  
 
-  context "name can't be empty" do 
-    let(:empty_name) { attributes_for(:empty_name_for_lawyer) }
-    subject { described_class.new(empty_name) }
+    context "name can't be empty" do 
+      let(:empty_name) { attributes_for(:empty_name_for_lawyer) }
+      subject { described_class.new(empty_name) }
 
-    it { expect { subject.perform }.not_to change{ Lawyer.count } }
-  end
+      it { expect { subject.perform }.not_to change{ Lawyer.count } }
+    end
 
-  context "email can't be empty" do 
-    let(:empty_email) { attributes_for(:empty_email_for_lawyer) }
-    subject { described_class.new(empty_email) }
-    
-    it { expect { subject.perform }.not_to change{ Lawyer.count } }
+    context "email can't be empty" do 
+      let(:empty_email) { attributes_for(:empty_email_for_lawyer) }
+      subject { described_class.new(empty_email) }
+      
+      it { expect { subject.perform }.not_to change{ Lawyer.count } }
+    end
   end
 end 
