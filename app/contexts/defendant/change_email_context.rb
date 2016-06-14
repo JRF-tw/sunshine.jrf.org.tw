d9d3420ac18f18a7feccbd47d4d639b20fc82e98
Aron <h0935674938@hotmail.com>
2016-06-14 13:52:37 +0800

    在 context 轉換 email to unconfirmed_email

--- app/contexts/defendant/change_email_context.rb
+++ app/contexts/defendant/change_email_context.rb
@@ -1,9 +1,10 @@
 class Defendant::ChangeEmailContext < BaseContext
-  PERMITS = [:unconfirmed_email, :current_password].freeze
+  PERMITS = [:email, :current_password].freeze
 
   before_perform :check_email_not_empty
   before_perform :check_email_different
   before_perform :check_email_unique
+  before_perform :transfer_email_to_unconfirmed_email
   after_perform :send_confirmation_email
   def initialize(defendant)
     @defendant = defendant
@@ -20,15 +21,19 @@ class Defendant::ChangeEmailContext < BaseContext
   private
 
   def check_email_not_empty
-    return add_error(:email_conflict, "email 不可為空") if @params["unconfirmed_email"] == ""
+    return add_error(:email_conflict, "email 不可為空") if @params["email"] == ""
   end
 
   def check_email_different
-    return add_error(:email_conflict, "email 不可與原本相同") if @params["unconfirmed_email"] == @defendant.email
+    return add_error(:email_conflict, "email 不可與原本相同") if @params["email"] == @defendant.email
   end
 
   def check_email_unique
-    return add_error(:email_conflict, "email 已經被使用") if Defendant.pluck(:email).include?(@params["unconfirmed_email"]) || Defendant.pluck(:unconfirmed_email).include?(@params["unconfirmed_email"])
+    return add_error(:email_conflict, "email 已經被使用") if Defendant.pluck(:email).include?(@params["email"]) || Defendant.pluck(:unconfirmed_email).include?(@params["email"])
+  end
+
+  def transfer_email_to_unconfirmed_email
+    @params["unconfirmed_email"] = @params.delete("email")
   end
 
   def send_confirmation_email
