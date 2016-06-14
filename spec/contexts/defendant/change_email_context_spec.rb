264e901e0ed51b4c7d5a024c1a6acb63b83e8607
Aron <h0935674938@hotmail.com>
2016-06-14 14:35:20 +0800

    rspec fix

--- spec/contexts/defendant/change_email_context_spec.rb
+++ spec/contexts/defendant/change_email_context_spec.rb
@@ -6,7 +6,7 @@ describe Defendant::ChangeEmailContext do
 
   describe "perform" do
     context "success" do
-      let(:params) { { unconfirmed_email: "h2312@gmail.com", current_password: "12321313213" } }
+      let(:params) { { email: "h2312@gmail.com", current_password: "12321313213" } }
       
 
       it { expect{ subject.perform(params) }.to change { defendant.reload.unconfirmed_email } }
@@ -14,29 +14,29 @@ describe Defendant::ChangeEmailContext do
 
     context "email already exist" do
       let!(:defendant2) { FactoryGirl.create :defendant, email: "55667788@gmail.com"   }
-      let(:params) { { unconfirmed_email: defendant2.email, current_password: "12321313213" } }
+      let(:params) { { email: defendant2.email, current_password: "12321313213" } }
       it { expect{ subject.perform(params) }.not_to change { defendant.reload.unconfirmed_email } }
     end
 
     context "empty email" do
-      let(:params) { { unconfirmed_email: "", current_password: "12321313213" } }
+      let(:params) { { email: "", current_password: "12321313213" } }
       it { expect{ subject.perform(params) }.not_to change { defendant.reload.unconfirmed_email } }
     end
     
     context "empty password" do
-      let(:params) { { unconfirmed_email: "h2312@gmail.com", current_password: "" } }
+      let(:params) { { email: "h2312@gmail.com", current_password: "" } }
       it { expect{ subject.perform(params) }.not_to change { defendant.reload.unconfirmed_email } }
     end
 
     context "update the same email" do
-      let(:params) { { unconfirmed_email: defendant.email, current_password: "12321313213" } }
+      let(:params) { { email: defendant.email, current_password: "12321313213" } }
 
       it { expect{ subject.perform(params) }.not_to change { defendant.reload.unconfirmed_email } }
     end
 
     context "update other's unconfirmed_email" do
       let!(:defendant2) { FactoryGirl.create :defendant, unconfirmed_email: "55667788@gmail.com"   }
-      let(:params) { { unconfirmed_email: defendant2.unconfirmed_email, current_password: "12321313213" } }
+      let(:params) { { email: defendant2.unconfirmed_email, current_password: "12321313213" } }
 
       it { expect{ subject.perform(params) }.not_to change { defendant.reload.unconfirmed_email } }
     end
