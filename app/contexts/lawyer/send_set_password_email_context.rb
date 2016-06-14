497b33aee5ae4d3f87ba5ed69fbb07d46e999666
Aron <h0935674938@hotmail.com>
2016-06-14 19:12:58 +0800

    修改 律師後台寄送重設密碼信 寫成 律師重寄認證信的錯誤

--- app/contexts/lawyer/send_set_password_email_context.rb
+++ app/contexts/lawyer/send_set_password_email_context.rb
@@ -1,22 +1,14 @@
 class Lawyer::SendSetPasswordEmailContext < BaseContext
 
-  before_perform :check_confirm
-
   def initialize(lawyer)
     @lawyer = lawyer
   end
 
   def perform
     run_callbacks :perform do
-      @lawyer.send_confirmation_instructions
+      @lawyer.send_reset_password_instructions
       return true
     end
-  end
-
-  private
-
-  def check_confirm
-    return add_error(:send_email_fail, "該律師已驗證") if @lawyer.confirmed?
   end
 
 end
