3d27d8efd3bc7daa555c887d06880d4b383ad3f7
phrase5036 <phrase5036@gmail.com>
2016-06-15 17:29:15 +0800

    rm useless migrations

--- db/migrate/20160614083429_add_column_unconfirm_phone_to_defendants.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnUnconfirmPhoneToDefendants < ActiveRecord::Migration
-  def change
-    add_column :defendants, :unconfirmed_phone, :string
-  end
-end
--- db/migrate/20160615090551_remove_column_unconfirmed_phone_to_defendants.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnUnconfirmedPhoneToDefendants < ActiveRecord::Migration
-  def up
-    remove_column :defendants, :unconfirmed_phone
-  end
-
-  def down
-    add_column :defendants, :unconfirmed_phone, :string
-  end
-end
