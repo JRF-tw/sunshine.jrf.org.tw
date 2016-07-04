88993c4adefbe0b0b94e3b9a2e1b60ae3d8407ac
Aron <h0935674938@hotmail.com>
2016-07-04 16:40:49 +0800

    修復重複登入問題

--- app/controllers/bystander/passwords_controller.rb
+++ app/controllers/bystander/passwords_controller.rb
@@ -15,6 +15,26 @@ class Bystander::PasswordsController < Devise::PasswordsController
     end
   end
 
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message(:notice, flash_message) if is_flashing_format?
+        sign_in(resource_name, resource) unless current_bystander
+      else
+        set_flash_message(:notice, :updated_not_active) if is_flashing_format?
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   def send_reset_password_mail
     context = Bystander::SendSetPasswordEmailContext.new(current_bystander)
     context.perform
--- app/controllers/lawyer/passwords_controller.rb
+++ app/controllers/lawyer/passwords_controller.rb
@@ -21,6 +21,26 @@ class Lawyer::PasswordsController < Devise::PasswordsController
     end
   end
 
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message(:notice, flash_message) if is_flashing_format?
+        sign_in(resource_name, resource) unless current_lawyer
+      else
+        set_flash_message(:notice, :updated_not_active) if is_flashing_format?
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   def edit
     if current_lawyer && Lawyer.with_reset_password_token(params[:reset_password_token]) != current_lawyer
       redirect_as_fail(lawyer_profile_path, "你僅能修改本人的帳號")
