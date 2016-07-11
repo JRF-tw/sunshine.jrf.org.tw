4b6bf50aacf5b47bbb3f1f62a99af885798bf55f
Aron <h0935674938@hotmail.com>
2016-07-11 15:51:51 +0800

    bug 修復  更改通知時間

--- config/schedule.rb
+++ config/schedule.rb
@@ -53,11 +53,11 @@ every 1.day, at: "6:00 am" do
 end
 
 # notify subscriber before judge
-every 1.day, at: "7:00 am" do
+every 1.day, at: "4:00 pm" do
   runner "Crontab::SubscribeStoryBeforeJudgeNotifyContext.new(Date.today).perform"
 end
 
 # notify subscriber after judge
-every 1.day, at: "7:00 am" do
+every 1.day, at: "4:00 pm" do
   runner "Crontab::SubscribeStoryAfterJudgeNotifyContext.new(Date.today).perform"
 end
