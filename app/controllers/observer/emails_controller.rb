c50ea8f49f21257d876b4d21109d912f31e26145
Aron <h0935674938@hotmail.com>
2016-07-12 15:35:34 +0800

    寄信功能 與 密碼變更功能 的修改

--- app/controllers/observer/emails_controller.rb
+++ app/controllers/observer/emails_controller.rb
@@ -1,5 +1,16 @@
 class Observer::EmailsController < Observer::BaseController
+
   def edit
-    # TODO: replace from observer registration edit
+  end
+
+  def update
+    context = CourtObserver::ChangeEmailContext.new(current_court_observer)
+    if context.perform(params)
+      flash[:notice] = "需要重新驗證新的Email"
+      redirect_to observer_profile_url
+    else
+      flash.now[:error] = context.error_messages.join(", ")
+      render :edit
+    end
   end
 end
