eb745153867e340bce80407d33eef51e8fc32358
Aron <h0935674938@hotmail.com>
2016-07-15 14:21:22 +0800

    rspec fix

--- spec/requests/party/emails_controller_spec.rb
+++ spec/requests/party/emails_controller_spec.rb
@@ -4,24 +4,25 @@ RSpec.describe Party::EmailsController, type: :request do
   before { signin_party }
 
   describe "#edit" do
-    before { signin_party }
     subject! { get "/party/email/edit" }
     it { expect(response).to be_success }
   end
 
   describe "#update" do
     context "success" do
-      before { signin_party }
-      subject! { put "/party/email", party: { email: "5566@gmail", current_password: "12321313213" } }
-      it { expect(response).to redirect_to("/party/profile") }
+      subject { put "/party/email", party: { email: "5566@gmail.com", current_password: "12321313213" } }
+
+      it { expect { subject }.to change_sidekiq_jobs_size_of(Devise::Async::Backend::Sidekiq) }
+      it { expect(subject).to redirect_to("/party/profile") }
     end
 
     context "wrong password" do
-      before { signin_party }
-      subject! { put "/party/email", party: { email: "5566@gmail", current_password: "" } }
+      subject { put "/party/email", party: { email: "5566@gmail.com", current_password: "" } }
 
-      it { expect { subject }.not_to change { current_party } }
-      it { expect(response.body).not_to match("目前等待驗證中信箱") }
+      it {
+        expect { subject }.not_to change { current_party }
+        expect(response.body).not_to match("目前等待驗證中信箱")
+      }
     end
   end
 end
