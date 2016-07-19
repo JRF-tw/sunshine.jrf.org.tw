a4cbce2b0a7dc26f92aa1d63eaf8f2679ce45665
Aron <h0935674938@hotmail.com>
2016-07-19 13:19:33 +0800

    律師後台 feature spec

---
+++ spec/features/後台 admin/律師後台/刪除律師_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe "律師後台 刪除律師", type: :request do
+  before { signin_user }
+  let!(:lawyer) { FactoryGirl.create :lawyer }
+
+  context "已經設定密碼的律師無法刪除" do
+    before { lawyer.confirm! }
+    subject { delete "/admin/lawyers/#{lawyer.id}", {}, "HTTP_REFERER" => "/admin/lawyers" }
+
+    it "刪除失敗" do
+      expect { subject }.not_to change { Lawyer.count }
+    end
+
+  end
+
+  context "尚未設定密碼的律師可以刪除" do
+    subject { delete "/admin/lawyers/#{lawyer.id}", {}, "HTTP_REFERER" => "/admin/lawyers" }
+
+    it "刪除成功" do
+      expect { subject }.to change { Lawyer.count }.by(-1)
+    end
+  end
+
+  context "刪除後回到原本的列表頁，假如原本是搜尋結果的某一頁，則必須回到該頁" do
+    subject! { delete "/admin/lawyers/#{lawyer.id}", {}, "HTTP_REFERER" => "/admin/lawyers?page=2" }
+
+    it "刪除成功" do
+      expect(response).to redirect_to("/admin/lawyers?page=2")
+    end
+  end
+end
