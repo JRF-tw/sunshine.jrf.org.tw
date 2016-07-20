a1036fb4c780c1d8157549498af8c75e0c1fefc5
phrase5036 <phrase5036@gmail.com>
2016-07-20 14:14:43 +0800

    fix up for spec error

--- spec/requests/parties/verdicts_controller_spec.rb
+++ spec/requests/parties/verdicts_controller_spec.rb
@@ -12,9 +12,4 @@ RSpec.describe Parties::VerdictsController, type: :request do
     subject! { get "/party/score/verdicts/rule" }
     it { expect(response).to be_success }
   end
-
-  describe "#verify" do
-    subject! { post "/party/score/verdicts/verify" }
-    it { expect(response).to be_redirect }
-  end
 end
