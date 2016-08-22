8d0152cb25dede8eb55cd0ff807bfecd108496d6
aronyu <h0935674938@hotmail.com>
2016-08-22 18:27:54 +0800

    wording 調整

--- app/mailers/lawyer_mailer.rb
+++ app/mailers/lawyer_mailer.rb
@@ -2,12 +2,15 @@ class LawyerMailer < ApplicationMailer
   def story_before_judge_notice(story_id, lawyer_id)
     @story = Story.find(story_id)
     @lawyer = Lawyer.find(lawyer_id)
-    mail(to: @lawyer.email, subject: "開庭通知")
+    @court_wording = "#{@story.court.full_name}#{@story.year}年#{@story.word_type}字第#{@story.number}號"
+    @date = @story.schedules.last.date
+    mail(to: @lawyer.email, subject: @court_wording + "開庭通知")
   end
 
   def story_after_judge_notice(story_id, lawyer_id)
     @story = Story.find(story_id)
     @lawyer = Lawyer.find(lawyer_id)
-    mail(to: @lawyer.email, subject: "評鑑通知")
+    @court_wording = "#{@story.court.full_name}#{@story.year}年#{@story.word_type}字第#{@story.number}號"
+    mail(to: @lawyer.email, subject: @court_wording + "開庭完畢，邀請您提供您的寶貴意見！")
   end
 end
--- app/mailers/party_mailer.rb
+++ app/mailers/party_mailer.rb
@@ -2,12 +2,15 @@ class PartyMailer < ApplicationMailer
   def story_before_judge_notice(story_id, party_id)
     @story = Story.find(story_id)
     @party = Party.find(party_id)
-    mail(to: @party.email, subject: "開庭通知")
+    @court_wording = "#{@story.court.full_name}#{@story.year}年#{@story.word_type}字第#{@story.number}號"
+    @date = @story.schedules.last.date
+    mail(to: @party.email, subject: @court_wording + "開庭通知")
   end
 
   def story_after_judge_notice(story_id, party_id)
     @story = Story.find(story_id)
     @party = Party.find(party_id)
-    mail(to: @party.email, subject: "評鑑通知")
+    @court_wording = "#{@story.court.full_name}#{@story.year}年#{@story.word_type}字第#{@story.number}號"
+    mail(to: @party.email, subject: @court_wording + "開庭完畢，邀請您提供您的寶貴意見！")
   end
 end
