b2a4668beeffce976cb53cfb61d800c327f41d42
phrase5036 <phrase5036@gmail.com>
2016-08-24 16:10:00 +0800

    fix rubocop

--- spec/support/capybara/common_helper.rb
+++ spec/support/capybara/common_helper.rb
@@ -1,9 +1,9 @@
 module Capybara
   module CommonHelper
     def manual_http_request(method, path)
-      page.execute_script("$.ajax({url: '#{path}', data: {}, type: '#{method.to_s}'});")
+      page.execute_script("$.ajax({url: '#{path}', data: {}, type: '#{method}'});")
       # Wait for the request to finish executing...
-      Timeout.timeout(10) { loop until page.evaluate_script('jQuery.active').zero? }
+      Timeout.timeout(10) { loop until page.evaluate_script("jQuery.active").zero? }
     end
   end
 end
