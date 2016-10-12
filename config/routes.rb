fed2f75d15a205f01740ee48007a527d81523ee4
phrase5036 <phrase5036@gmail.com>
2016-10-12 16:28:25 +0800

    fix by rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   mount RedactorRails::Engine => "/redactor_rails"
 
-  load Rails.root.join('config/routes/devise.rb')
-  load Rails.root.join('config/routes/f2e.rb')
+  load Rails.root.join("config/routes/devise.rb")
+  load Rails.root.join("config/routes/f2e.rb")
 
   unless Rails.env.production?
     # ver2 routes
-    load Rails.root.join('config/routes/devise_ver2.rb')
-    load Rails.root.join('config/routes/f2e_ver2.rb')
-    load Rails.root.join('config/routes/f2e_observer.rb')
-    load Rails.root.join('config/routes/f2e_lawyer.rb')
-    load Rails.root.join('config/routes/f2e_party.rb')
+    load Rails.root.join("config/routes/devise_ver2.rb")
+    load Rails.root.join("config/routes/f2e_ver2.rb")
+    load Rails.root.join("config/routes/f2e_observer.rb")
+    load Rails.root.join("config/routes/f2e_lawyer.rb")
+    load Rails.root.join("config/routes/f2e_party.rb")
   end
 
-  load Rails.root.join('config/routes/api.rb')
-  load Rails.root.join('config/routes/admin.rb')
+  load Rails.root.join("config/routes/api.rb")
+  load Rails.root.join("config/routes/admin.rb")
 end
