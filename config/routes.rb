e98bcbf05c3f9c2a55321176aee34d4a5421245a
phrase5036 <phrase5036@gmail.com>
2016-10-12 16:17:58 +0800

    hide routes for prodution

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   load Rails.root.join('config/routes/devise.rb')
   load Rails.root.join('config/routes/f2e.rb')
-  load Rails.root.join('config/routes/f2e_observer.rb')
-  load Rails.root.join('config/routes/f2e_lawyer.rb')
-  load Rails.root.join('config/routes/f2e_party.rb')
+
+  unless Rails.env.production?
+    load Rails.root.join('config/routes/f2e_observer.rb')
+    load Rails.root.join('config/routes/f2e_lawyer.rb')
+    load Rails.root.join('config/routes/f2e_party.rb')
+  end
+
   load Rails.root.join('config/routes/api.rb')
   load Rails.root.join('config/routes/admin.rb')
 end
