730ba809e7da8d0d3f9b87c38687900234014d01
aronyu <h0935674938@hotmail.com>
2016-10-13 17:17:39 +0800

    capybara 架構

--- spec/features/前台帳號功能/律師/登入_spec.rb
+++ spec/features/前台帳號功能/律師/登入_spec.rb
@@ -1,57 +1,38 @@
 require "rails_helper"
 
-describe "律師登入", type: :request do
-  let!(:lawyer) { create :lawyer, :with_password, :with_confirmed }
+feature "前台帳號功能", type: :feature, js: true do
+  feature "律師" do
+    feature "登入" do
+      Scenario "律師在完成註冊後進行登入" do
+        Given "無律師登入" do
+          When "律師登入" do
+            Then "登入成功" do
+            end
+          end
+        end
 
-  context "成功登入" do
-    subject { post "/lawyer/sign_in", lawyer: { email: lawyer.email, password: lawyer.password } }
-
-    it "導到個人評鑑紀錄頁" do
-      expect(subject).to redirect_to("/lawyer")
-    end
-  end
-
-  context "失敗登入" do
-    context "email不存在" do
-      before { post "/lawyer/sign_in", lawyer: { email: "aaaa22@gamil.com", password: "password" } }
-      it "顯示 輸入資訊是無效的" do
-        expect(flash[:alert]).to eq("輸入資訊是無效的。")
+        Given "有律師登入" do
+          When "律師登入" do
+            Then "登入失敗，頁面轉跳" do
+            end
+          end
+        end
       end
-    end
 
-    context "email 密碼皆正確，但該帳號尚未設定密碼" do
-      let!(:lawyer_without_confirmed) { create :lawyer, :with_password }
-      before { post "/lawyer/sign_in", lawyer: { email: lawyer_without_confirmed.email, password: lawyer_without_confirmed.password } }
-      it "顯示 您的帳號需需要經過驗證後，才能繼續。" do
-        expect(flash[:alert]).to eq("您的帳號需需要經過驗證後，才能繼續。")
-      end
-    end
+      Scenario "律師尚未完成註冊進行登入" do
+        Given "資料庫有律師資料，但尚未完成註冊" do
+          When "律師登入，密碼為預設密碼" do
+            Then "登入失敗，頁面轉跳" do
+            end
+          end
+        end
 
-    context "email 格式不符" do
-      before { post "/lawyer/sign_in", lawyer: { email: "aaaa2", password: lawyer.password } }
-      it "顯示 輸入資訊是無效的" do
-        expect(flash[:alert]).to eq("輸入資訊是無效的。")
-      end
-    end
-
-    context "email 空白" do
-      before { post "/lawyer/sign_in", lawyer: { email: "", password: lawyer.password } }
-      it "顯示 信箱或密碼是無效的。" do
-        expect(flash[:alert]).to eq("信箱或密碼是無效的。")
-      end
-    end
-
-    context "密碼空白 + email 正確" do
-      before { post "/lawyer/sign_in", lawyer: { email: lawyer.email, password: "" } }
-      it "顯示 輸入資訊是無效的" do
-        expect(flash[:alert]).to eq("輸入資訊是無效的。")
-      end
-    end
-
-    context "密碼不正確 + email 正確" do
-      before { post "/lawyer/sign_in", lawyer: { email: lawyer.email, password: "ppwwppww" } }
-      it "失敗後轉跳的頁面仍須保留原本輸入的 email" do
-        expect(response.body).to match(lawyer.email)
+        Given "律師登入密碼為預設密碼" do
+          When "律師登入，密碼為預設密碼" do
+            Then "登入失敗，頁面轉跳" do
+            end
+          end
+        end
       end
     end
   end
