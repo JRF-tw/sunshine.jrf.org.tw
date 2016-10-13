66ef638be6c52433a7856ec186b5206fdd3c869c
aronyu <h0935674938@hotmail.com>
2016-10-13 19:07:58 +0800

    review fix

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,11 +3,11 @@ module AdminHelper
     render partial: "admin/base/widget_box", locals: { main: capture(&block), title: title, icon: icon }
   end
 
-  def sort_button(court)
+  def sort_buttons(court)
     url_array = []
     { first: "頂", up: "上", down: "下", last: "底" }.each do |weight, label|
       url_array << link_to(label, admin_court_update_weight_path(court, admin_court: { weight: weight }, format: :js), class: "btn btn-mini btn-info", remote: true, method: :put)
     end
-    url_array
+    safe_join(url_array, " ")
   end
 end
