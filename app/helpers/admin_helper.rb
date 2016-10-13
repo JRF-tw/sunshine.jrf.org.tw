ca239e96ba0a4419837967bc9af6d21d1194b81b
aronyu <h0935674938@hotmail.com>
2016-10-13 18:03:40 +0800

    sort_link generate 移到helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,12 @@ module AdminHelper
   def admin_widget_box(title, icon: nil, &block)
     render partial: "admin/base/widget_box", locals: { main: capture(&block), title: title, icon: icon }
   end
+
+  def sort_button(court)
+    url_array = []
+    { first: "頂", up: "上", down: "下", last: "底" }.each do |weight, label|
+      url_array << link_to(label, admin_court_update_weight_path(court, admin_court: { weight: weight }, format: :js), class: "btn btn-mini btn-info", remote: true, method: :put)
+    end
+    url_array
+  end
 end
