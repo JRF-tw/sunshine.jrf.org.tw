931dfa42e478eb4f6bf3f0cda509f302e42a8670
aronyu <h0935674938@hotmail.com>
2016-10-14 16:04:18 +0800

    test case fix

--- spec/features/前台帳號功能/律師/登入_spec.rb
+++ spec/features/前台帳號功能/律師/登入_spec.rb
@@ -28,19 +28,19 @@ feature "前台帳號功能", type: :feature, js: true do
       end
 
       Scenario "律師尚未完成註冊進行登入" do
-        Given "資料庫有律師資料，但尚未完成註冊" do
-          let!(:lawyer) { create :lawyer }
-          When "律師登入，密碼為預設密碼" do
+        Given "資料庫有律師資料，包含登入帳號和密碼，但尚未完成註冊" do
+          let!(:lawyer) { create :lawyer, :with_password }
+          When "律師以正確的帳號資訊進行登入" do
             before { capybara_signin_lawyer(email: lawyer.email, password: "123123123") }
             Then "登入失敗，頁面轉跳" do
               expect(current_path).to match(lawyer_root_path)
-              expect(page).to have_content("信箱或密碼是無效的。")
+              expect(page).to have_content("您的帳號需要經過驗證後，才能繼續。")
             end
           end
         end
 
         Given "資料庫無該律師資料" do
-          When "律師登入，密碼為預設密碼" do
+          When "律師登入" do
             before { capybara_signin_lawyer(email: "5566@gmail.com", password: "123123123") }
             Then "登入失敗，頁面轉跳" do
               expect(current_path).to match(lawyer_root_path)
