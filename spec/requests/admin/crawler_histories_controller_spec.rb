41b89c635715895a40a91340c6bc4a51451370ca
phrase5036 <phrase5036@gmail.com>
2016-10-20 16:12:40 +0800

    add request rspec

--- spec/requests/admin/crawler_histories_controller_spec.rb
+++ spec/requests/admin/crawler_histories_controller_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
-RSpec.describe Admin::CrawlerHistoriesController, type: :controller do
+RSpec.describe Admin::CrawlerHistoriesController, type: :request do
+  before { signin_user }
+  let!(:crawler_history) { create :crawler_history }
 
+  describe "#index" do
+    context "render success" do
+      before { get "/admin/crawler_histories" }
+      it { expect(response).to be_success }
+    end
+  end
 end
