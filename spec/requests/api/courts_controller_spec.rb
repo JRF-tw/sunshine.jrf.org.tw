780ef134f0f524b898b1b2c5d7f386c0363e0aae
aronyu <h0935674938@hotmail.com>
2016-10-25 17:35:58 +0800

    補上 狀態與 data 測試

--- spec/requests/api/courts_controller_spec.rb
+++ spec/requests/api/courts_controller_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::CourtsController, type: :request do
 
   describe '#index' do
+    let!(:court) { create :court }
+    def response_body
+      JSON.parse(response.body)
+    end
+
     context 'success' do
       subject! { get '/api/courts.json' }
-      it { expect(response).to be_success }
+      it { expect(response_body[0]['full_name']).to match(court.full_name) }
+      it { expect(response.status).to eq 200 }
     end
 
     context 'undefined route' do
