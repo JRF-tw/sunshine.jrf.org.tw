86fab4c3645c0689e416726f17c3cb05238ea96d
phrase5036 <phrase5036@gmail.com>
2016-10-26 16:48:58 +0800

    打開爬取法院記錄log

--- app/contexts/scrap/get_courts_context.rb
+++ app/contexts/scrap/get_courts_context.rb
@@ -10,6 +10,10 @@ class Scrap::GetCourtsContext < BaseContext
     end
   end
 
+  def initialize
+    @crawler_history = CrawlerHistory.find_or_create_by(crawler_on: Time.zone.today)
+  end
+
   def perform
     run_callbacks :perform do
       @scrap_data.each do |court_data|
@@ -28,13 +32,15 @@ class Scrap::GetCourtsContext < BaseContext
       @scrap_data << { scrap_name: data.text, code: data.attr('value').gsub(data.text, '').squish }
     end
   rescue
+    Logs::AddCrawlerError.parse_court_data_error(@crawler_history, :crawler_failed, "回傳資訊取得失敗 : 來源網址 #{SCRAP_URI}")
     nil
   end
 
   def parse_courts_data(response_data)
     return response_data.css('table')[2].css('select')[0].css('option')
   rescue => e
-    SlackService.notify_scrap_court_error("法院爬取失敗: 解析法院代碼資訊錯誤\n #{e.message}")
+    Logs::AddCrawlerError.parse_court_data_error(@crawler_history, :parse_data_failed, "擷取規則錯誤 : 無法取得所有法院資訊")
+    false
   end
 
   def check_db_data_and_notify
--- app/lib/logs/add_crawler_error.rb
+++ app/lib/logs/add_crawler_error.rb
@@ -8,5 +8,11 @@ class Logs::AddCrawlerError
       crawler_log.crawler_errors << error_message unless crawler_log.crawler_errors.include?(error_message)
       crawler_log.save
     end
+
+    def parse_court_data_error(crawler_history, type, message)
+      crawler_log = crawler_history.crawler_logs.find_or_create_by(crawler_kind: CrawlerKinds.list.keys.index(:crawler_court), crawler_error_type: CrawlerErrorTypes.list.keys.index(type))
+      crawler_log.crawler_errors << message unless crawler_log.crawler_errors.include?(message)
+      crawler_log.save
+    end
   end
 end
