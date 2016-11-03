42f37827aea978a9f617348011cd91d5d0db15bc
aronyu <h0935674938@hotmail.com>
2016-11-03 12:23:03 +0800

    更改 controller 接住 重複email 的例外

--- app/controllers/parties/confirmations_controller.rb
+++ app/controllers/parties/confirmations_controller.rb
@@ -3,9 +3,15 @@ class Parties::ConfirmationsController < Devise::ConfirmationsController
   before_action :redirect_new_to_sign_in, only: [:new]
 
   def show
-    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-    yield resource if block_given?
+    party = Party.find_by_confirmation_token(params[:confirmation_token])
+    if party && Party.pluck(:email).include?(party.unconfirmed_email)
+      set_flash_message(:alert, :conflict_confirmed)
+      return redirect_to new_party_session_path
+    else
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    end
 
+    yield resource if block_given?
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_flashing_format?
       respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
