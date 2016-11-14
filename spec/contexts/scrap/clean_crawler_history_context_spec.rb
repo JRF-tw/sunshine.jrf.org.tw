46d50c69e7a0a6ea9b77e6b3c12c3d5ed843ff14
phrase5036 <phrase5036@gmail.com>
2016-11-15 01:03:05 +0800

    保留一個月的爬蟲紀錄

---
+++ spec/contexts/scrap/clean_crawler_history_context_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Scrap::CleanCrawlerHistoryContext do
+  before { create :crawler_history }
+  before { create :crawler_history, crawler_on: Time.zone.today - 2.month }
+
+  describe 'perform' do
+    context 'success' do
+      subject { described_class.new.perform }
+      it { expect { subject }.to change { CrawlerHistory.count } }
+    end
+  end
+end
