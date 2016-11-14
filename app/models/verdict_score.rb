da43541fd81d749fa52088760d8a3843d0399cdb
phrase5036 <phrase5036@gmail.com>
2016-11-14 12:24:46 +0800

    change score columns

--- app/models/verdict_score.rb
+++ app/models/verdict_score.rb
@@ -6,13 +6,12 @@
 #  story_id           :integer
 #  verdict_rater_id   :integer
 #  verdict_rater_type :string
-#  quality_score      :float
-#  rating_score       :float
 #  data               :hstore
 #  appeal_judge       :boolean
 #  status             :integer
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
+#  quality_scores     :hstore
 #
 
 class VerdictScore < ActiveRecord::Base
@@ -20,6 +19,7 @@ class VerdictScore < ActiveRecord::Base
   validates :verdict_rater_id, presence: true, uniqueness: { scope: [:verdict_rater_type, :story_id] }
   validates :story_id, presence: true
   store_accessor :data, :court_id, :year, :word_type, :number, :story_type, :note
+  store_accessor :quality_score, :score_3_1, :score_3_2_1, :score_3_2_2, :score_3_2_3, :score_3_2_4, :score_3_2_5, :score_3_2_6
 
   belongs_to :story
   belongs_to :verdict_rater, polymorphic: true
--- spec/factories/schedule_scores.rb
+++ spec/factories/schedule_scores.rb
@@ -7,14 +7,13 @@
 #  judge_id            :integer
 #  schedule_rater_id   :integer
 #  schedule_rater_type :string
-#  rating_score        :float
-#  command_score       :float
-#  attitude_score      :float
 #  data                :hstore
 #  appeal_judge        :boolean          default(FALSE)
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #  story_id            :integer
+#  attitude_scores     :hstore
+#  command_scores      :hstore
 #
 
 FactoryGirl.define do
