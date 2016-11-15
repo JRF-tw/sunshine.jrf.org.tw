3a69aa5afe50bf0b3c93baeca80a774fca77cd8f
aronyu <h0935674938@hotmail.com>
2016-11-15 15:56:35 +0800

    code review fix

--- app/mailers/party_mailer.rb
+++ app/mailers/party_mailer.rb
@@ -13,9 +13,15 @@ class PartyMailer < ApplicationMailer
     @story = Story.find(story_id)
     @party = Party.find(party_id)
     @token = @party.unsubscribe_token
-    @params_for_schedule_score = { schedule_score: { court_id: @story.court, year: @story.year, word_type: @story.word_type, number: @story.number, story_type: @story.story_type, start_on: @story.schedules.last.start_on } }
+    @score_schedules_link = generate_score_schedules_link(@story)
     @close_court_info_wording = MailerPresenters.new.remind_story_after_judge(@story)
     @subject = @story.detail_info + '開庭完畢，邀請您提供您的寶貴意見！'
     mail(to: @party.email, subject: @subject)
+  end
+
+  private
+
+  def generate_score_schedules_link(story)
+    input_judge_party_score_schedules_url(schedule_score: { court_id: story.court, year: story.year, word_type: story.word_type, number: story.number, story_type: story.story_type, start_on: story.schedules.last.start_on })
   end
 end
