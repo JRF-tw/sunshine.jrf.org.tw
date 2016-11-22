af3471ec9a735fd2da8419ca8f717443a9b98c8a
aronyu <h0935674938@hotmail.com>
2016-11-22 15:46:23 +0800

    bulletin model 測試

---
+++ spec/models/bulletin_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Bulletin, type: :model do
+  let!(:bulletin) { create :bulletin }
+
+  it 'FactoryGirl' do
+    expect(bulletin).not_to be_new_record
+  end
+
+  describe '#can_be_banner' do
+    context 'true' do
+      let!(:bulletin) { create :bulletin, :with_pic }
+      it { expect(bulletin.can_be_banner?).to be_truthy }
+    end
+
+    context 'false' do
+      let!(:bulletin) { create :bulletin }
+      it { expect(bulletin.can_be_banner?).to be_falsey }
+    end
+  end
+end
