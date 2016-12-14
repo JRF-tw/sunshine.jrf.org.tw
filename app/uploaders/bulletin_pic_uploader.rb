f2e3b2aba802a926622e814cbd71f6b827e74e4e
aronyu <h0935674938@hotmail.com>
2016-12-14 16:55:31 +0800

    resize bulletin image

--- app/uploaders/bulletin_pic_uploader.rb
+++ app/uploaders/bulletin_pic_uploader.rb
@@ -1,15 +1,11 @@
 # encoding: utf-8
 
 class BulletinPicUploader < BaseUploader
-  version :S_280 do
-    process resize_to_fill: [280, 140]
+  version :W_255 do
+    process resize_to_fill: [255, 170]
   end
 
-  version :S_540 do
-    process resize_to_fill: [540, 270]
-  end
-
-  version :S_780 do
-    process resize_to_fill: [780, 390]
+  version :W_656 do
+    process resize_to_fill: [656, 437]
   end
 end
--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -40,6 +40,30 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
     process resize_to_limit: [800, 800]
   end
 
+  version :W_288 do
+    process resize_to_fill: [288, 158]
+  end
+
+  version :W_722 do
+    process resize_to_limit: [722, 401]
+  end
+
+  version :W_1018 do
+    process resize_to_fill: [1018, 565]
+  end
+
+  version :W_1260 do
+    process resize_to_limit: [1260, 700]
+  end
+
+  version :W_1501 do
+    process resize_to_limit: [1501, 833]
+  end
+
+  version :W_1706 do
+    process resize_to_limit: [1706, 947]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
