8f98ec20e6024a6524146d3f5eb6a5441aa5bf18
aronyu <h0935674938@hotmail.com>
2016-12-15 13:10:10 +0800

    creditor button adjust

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -33,6 +33,6 @@ CKEDITOR.editorConfig = function( config ) {
     { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
     { name: 'about', items: [ 'About' ] }
   ];
-  config.removeButtons = 'Save,Flash,Smiley,Iframe,Anchor,CreateDiv';
-  config.removePlugins = "forms";
+  config.removeButtons = 'Save,NewPage,Preview,Print,Scayt,Templates,PasteFromWord,Language,Flash,Smiley,PageBreak,Iframe,Anchor,CreateDiv';
+  config.removePlugins = "forms,about";
 };
