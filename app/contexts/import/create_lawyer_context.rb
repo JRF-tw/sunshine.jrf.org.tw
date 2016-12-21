e4a5ab756b878f267987c325ef56d3f57f3fc417
aronyu <h0935674938@hotmail.com>
2016-12-21 18:40:54 +0800

    解析奇怪的 phone 格式

--- app/contexts/import/create_lawyer_context.rb
+++ app/contexts/import/create_lawyer_context.rb
@@ -1,10 +1,9 @@
 class Import::CreateLawyerContext < BaseContext
+  before_perform  :check_has_email
   before_perform  :check_email_not_exist
   before_perform  :check_has_name
-  before_perform  :check_has_email
-  before_perform  :phone_format_check
-  before_perform  :assign_phone_number, if: :is_phone?
-  before_perform  :assign_office_number, unless: :is_phone?
+  before_perform  :assign_phone, if: :phone_exist?
+  before_perform  :clean_phone_params
   before_perform  :build_lawyer
 
   def initialize(lawyer_data)
@@ -20,39 +19,64 @@ class Import::CreateLawyerContext < BaseContext
 
   private
 
+  def check_has_email
+    return add_error(:lawyer_email_blank) unless @lawyer_data[:email].present?
+  end
+
   def check_email_not_exist
-    return add_error(:lawyer_exist) if Lawyer.pluck(:email).include?(@lawyer_data[:email])
+    return add_error(:lawyer_exist) if Lawyer.pluck(:email).include?(@lawyer_data[:email].downcase)
   end
 
   def check_has_name
     return add_error(:lawyer_name_blank) unless @lawyer_data[:name].present?
   end
 
-  def check_has_email
-    return add_error(:lawyer_email_blank) unless @lawyer_data[:email].present?
+  def assign_phone
+    phone_format_adjust
+    if @lawyer_data[:phone].size >= 20
+      multi_phones_assign
+    else
+      single_phone_assign
+    end
   end
 
-  def phone_format_check
-    phone_format_adjust
-    @lawyer_data[:phone] = '0' + @lawyer_data[:phone] if @lawyer_data[:phone].present? && @lawyer_data[:phone][0] != '0'
+  def phone_exist?
+    @lawyer_data[:phone].present?
   end
 
   def phone_format_adjust
     @lawyer_data[:phone] = @lawyer_data[:phone].to_s
-    @lawyer_data[:phone].gsub!(/[\s()-]/, '')
+    @lawyer_data[:phone] = '0' + @lawyer_data[:phone] if @lawyer_data[:phone].present? && @lawyer_data[:phone][0] != '0'
+    @lawyer_data[:phone].gsub!(/\(|\)|-|\s/, '')
     @lawyer_data[:phone].gsub!(/[(分機)]/, '#')
   end
 
-  def is_phone?
-    @lawyer_data[:phone] && @lawyer_data[:phone][0, 2] == '09'
+  def multi_phones_assign
+    number_1 = @lawyer_data[:phone].slice!(0..9)
+    number_2 = @lawyer_data[:phone]
+    if number_1[0, 2] == '09'
+      assign_phone_number(number_1)
+      assign_office_number(number_2)
+    else
+      assign_phone_number(number_2)
+      assign_office_number(number_1)
+    end
   end
 
-  def assign_phone_number
-    @lawyer_data[:phone_number] = @lawyer_data.delete(:phone)
+  def single_phone_assign
+    @lawyer_data[:phone][0, 2] == '09' ? assign_phone_number : assign_office_number
   end
 
-  def assign_office_number
-    @lawyer_data[:office_number] = @lawyer_data.delete(:phone)
+  def assign_phone_number(phone_number = nil)
+    @lawyer_data[:phone_number] = phone_number || @lawyer_data[:phone]
+  end
+
+  def assign_office_number(office_number = nil)
+    @lawyer_data[:office_number] = office_number || @lawyer_data[:phone]
+  end
+
+  def clean_phone_params
+    @lawyer_data.delete(:phone)
   end
 
   def build_lawyer
