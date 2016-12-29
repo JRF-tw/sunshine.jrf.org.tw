cd8bc5ac3f9273dc101101867539890027d1bc62
aronyu <h0935674938@hotmail.com>
2016-12-29 13:28:34 +0800

    暫時移除  開庭前通知

--- app/workers/subscriber_before_judge_notice_worker.rb
+++ app/workers/subscriber_before_judge_notice_worker.rb
@@ -3,11 +3,11 @@ class SubscriberBeforeJudgeNoticeWorker
   include Sidetiq::Schedulable
   sidekiq_options retry: 3
 
-  recurrence backfill: true do
-    daily.hour_of_day(17).minute_of_hour(30)
-  end
+  # recurrence backfill: true do
+  #   daily.hour_of_day(17).minute_of_hour(30)
+  # end
 
-  def perform
-    Crontab::SubscribeStoryBeforeJudgeNotifyContext.new(Time.zone.today).perform
-  end
+  # def perform
+  #   Crontab::SubscribeStoryBeforeJudgeNotifyContext.new(Time.zone.today).perform
+  # end
 end
