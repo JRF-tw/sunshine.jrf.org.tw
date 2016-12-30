ba71a566e83a40a4bb33dc8cc6aa16180af4b9e6
judewang <jude@jude.asia>
2016-12-30 23:47:35 +0800

    remove classic assets

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module JrfSunny
     config.i18n.default_locale = "zh-TW"
     config.i18n.fallbacks = [:en]
 
-    PRECOMPILE_FILES = ['priority.js', 'classic.js', 'classic.css']
+    PRECOMPILE_FILES = ['priority.js']
     # Make public assets requireable in manifest files
     # config.assets.paths << Rails.root.join("public", "assets", "stylesheets")
     # config.assets.paths << Rails.root.join("public", "assets", "javascripts")
