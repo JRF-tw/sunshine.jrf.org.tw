5cc5720d4c2ace5a5bdf9ef52265e0fcbd48d421
aronyu <h0935674938@hotmail.com>
2017-01-11 15:51:49 +0800

    helper bug fix and refactor

--- app/helpers/template_helper.rb
+++ app/helpers/template_helper.rb
@@ -20,9 +20,10 @@ module TemplateHelper
   def rwd_generate(html_content)
     content = html_content
 
-    all_image_tags = html_content.scan(/<img\salt.+\/>/)
-    all_image_tags.each do |img_tag|
-      content = content.gsub(img_tag, img_html_generate(img_tag))
+    if all_image_tags = html_content.scan(/<img\salt.+\/>/)
+      all_image_tags.each do |img_tag|
+        content = content.gsub(img_tag, img_html_generate(img_tag))
+      end
     end
     content
   end
@@ -31,16 +32,12 @@ module TemplateHelper
 
   def img_html_generate(image_tag)
     image_url = image_tag[/\/\/.+\.(jpg|png)/]
-    "<img alt='' class=lazyload data-sizes=auto data-srcset=#{all_size_srcset(image_url)} src=#{image_url} srcset=#{all_size_srcset(image_url)}/>"
+    "<img alt='' class='lazyload' data-sizes='auto' data-srcset=\"#{all_size_srcset(image_url)}\" src=\"#{image_url}\"/>"
   end
 
   def all_size_srcset(image_url)
-    src = "#{img_resize_url(image_url, '360')} 360W"
-    weights = %w(540 720 900 1080 1296 1512 1728 1944 2160 2592)
-    weights.each do |w|
-      src += ", #{img_resize_url(image_url, w)} #{w}W"
-    end
-    src
+    weights = %w(360 540 720 900 1080 1296 1512 1728 1944 2160 2592)
+    weights.map { |w| "#{img_resize_url(image_url, w)} #{w}w" }.join(',')
   end
 
   def img_resize_url(image_url, size)
