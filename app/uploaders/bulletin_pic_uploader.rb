720a0d7cb7edf47106e9f88d185a817da2946030
aronyu <h0935674938@hotmail.com>
2017-01-11 15:07:04 +0800

    review fix

--- app/uploaders/bulletin_pic_uploader.rb
+++ app/uploaders/bulletin_pic_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class BulletinPicUploader < BaseUploader
-  weights = %w(360 540 720 900 1080 1296 1512 1728)
+  weights = %w(360 720)
   weights.each do |w|
     w = w.to_i
     version "W_#{w}".to_sym do
