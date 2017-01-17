69bd7750a8e6a5ae9fc21fa1543bb78c73fd96b0
aronyu <h0935674938@hotmail.com>
2017-01-17 17:07:34 +0800

    最高法院法官 爬蟲context

---
+++ app/contexts/scrap/get_supreme_court_judges_context.rb
@@ -0,0 +1,73 @@
+class Scrap::GetSupremeCourtJudgesContext < BaseContext
+  SCRAP_URI = 'http://tps.judicial.gov.tw/about/index.php?parent_id=825'.freeze
+
+  before_perform  :get_judges_data
+  after_perform   :get_diff_import_daily_branch
+  after_perform   :notify_diff_info
+  after_perform   :update_diff_branch
+
+  class << self
+    def perform
+      new.perform
+    end
+  end
+
+  def initialize
+    @crawler_history = CrawlerHistory.find_or_create_by(crawler_on: Time.zone.today)
+    @court = Court.find_or_create_by(full_name: '最高法院', name: '最高院', code: 'TPS')
+  end
+
+  def perform
+    run_callbacks :perform do
+      @civil_brance_judge_names.each do |data_string|
+        Scrap::ImportSupremeCourtJudgesContext.new(data_string, '民事庭').perform
+      end
+
+      @criminal_brance_judge_names.each do |data_string|
+        Scrap::ImportSupremeCourtJudgesContext.new(data_string, '邢事庭').perform
+      end
+    end
+  end
+
+  private
+
+  def get_judges_data
+    response_data = Mechanize.new.get(SCRAP_URI)
+    response_data = Nokogiri::HTML(response_data.body)
+    @civil_brance_judge_names = parse_judges_data(response_data)[:civil]
+    @criminal_brance_judge_names = parse_judges_data(response_data)[:criminal]
+  rescue
+    request_retry(key: "#{SCRAP_URI} / #{Time.zone.today}")
+  end
+
+  def parse_judges_data(response_data)
+    { civil: response_data.css('table')[0].css('b').text.squish.split(')'), criminal: response_data.css('table')[1].css('b').text.squish.split(')') }
+  end
+
+  def request_retry(key:)
+    redis_object = Redis::Counter.new(key, expiration: 1.day)
+    if redis_object.value < 12
+      self.class.delay_for(1.hour).perform
+      redis_object.incr
+    else
+      Logs::AddCrawlerError.parse_judge_data_error(@crawler_history, :crawler_failed, "爬取最高法院法官錯誤, 來源網址 #{SCRAP_URI}")
+    end
+    false
+  end
+
+  def get_diff_import_daily_branch
+    @diff_branch_ids = @court.branches.map(&:id) - Redis::List.new('import_supreme_branch_ids').values.map(&:to_i)
+  end
+
+  def notify_diff_info
+    @diff_branch_ids.each do |d|
+      branch = Branch.find(d)
+      SlackService.notify_branch_alert("該股別不再目前爬蟲資料內 :\n法院名稱 : #{branch.court.full_name}\n名稱 : #{branch.name}\n 法庭名稱 : #{branch.chamber_name}")
+    end
+  end
+
+  def update_diff_branch
+    Branch.where(id: @diff_branch_ids).update_all(missed: true)
+    Redis::List.new('import_supreme_branch_ids').clear
+  end
+end
---
+++ app/contexts/scrap/import_supreme_court_judges_context.rb
@@ -0,0 +1,43 @@
+class Scrap::ImportSupremeCourtJudgesContext < BaseContext
+  before_perform  :parse_data
+  before_perform  :build_judge
+  after_perform   :import_branch
+  after_perform   :record_import_daily_branch
+  after_perform   :record_count_to_daily_notify
+
+  def initialize(data_string, chamber_name)
+    @crawler_history = CrawlerHistory.find_or_create_by(crawler_on: Time.zone.today)
+    @data_string = data_string
+    @court = Court.find_by(full_name: '最高法院', name: '最高院', code: 'TPS')
+    @chamber_name = chamber_name
+  end
+
+  def perform
+    run_callbacks :perform do
+      return add_error(:scrap_judge_create_fail) unless @judge.save
+      @judge
+    end
+  end
+
+  private
+
+  def parse_data
+    @judge_name, @branch_name = @data_string.split('(')
+  end
+
+  def build_judge
+    @judge = @court.judges.find_or_initialize_by(name: @judge_name)
+  end
+
+  def import_branch
+    @branch = Scrap::ImportBranchContext.new(@judge).perform(@chamber_name, @branch_name)
+  end
+
+  def record_import_daily_branch
+    Redis::List.new('import_supreme_branch_ids') << @branch.id
+  end
+
+  def record_count_to_daily_notify
+    Redis::Counter.new('daily_scrap_judge_count').increment
+  end
+end
