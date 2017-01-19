056c2261d1f88df44ce097acb49e0e153b789e99
aronyu <h0935674938@hotmail.com>
2017-01-19 12:17:29 +0800

    banner remove unless pic column

--- app/models/admin/banner.rb
+++ app/models/admin/banner.rb
@@ -3,9 +3,6 @@
 # Table name: banners
 #
 #  id          :integer          not null, primary key
-#  pic_l       :string
-#  pic_m       :string
-#  pic_s       :string
 #  weight      :integer
 #  is_hidden   :boolean
 #  created_at  :datetime
