447ba00b84f92c12bd63335444da6eb3b0cc23fb
aronyu <h0935674938@hotmail.com>
2017-02-09 15:53:58 +0800

    fix

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -1,6 +1,5 @@
 class BulletinsController < BaseController
   before_action :init_meta, only: [:index]
-  before_action :http_auth_for_production
 
   def index
     @bulletins = Bulletin.all.page(params[:page]).per(9)
