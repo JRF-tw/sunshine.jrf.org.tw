fe06b427a421285b357318786b4547befe6d72cc
aronyu <h0935674938@hotmail.com>
2017-02-16 13:02:35 +0800

    rspec fix

---
+++ spec/presenters/admin/score_presenters_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Admin::ScorePresenters do
+  let!(:verdict_score) { create :verdict_score }
+  let!(:schedule_score) { create :schedule_score }
+  let(:params) { attributes_for :score_search_form_object }
+  subject { described_class.new(params) }
+
+  describe '#result' do
+    context 'search rater' do
+      let!(:same_rater_schedule_score) { create :schedule_score, schedule_rater: verdict_score.verdict_rater }
+      before {
+        params[:rater_type_eq] = verdict_score.verdict_rater.class.to_s
+        params[:rater_id_eq] = verdict_score.verdict_rater.id
+      }
+      it { expect(subject.sorted_score.include?(verdict_score)).to be_truthy }
+      it { expect(subject.sorted_score.include?(same_rater_schedule_score)).to be_truthy }
+    end
+
+    context 'search score_type' do
+      before { params[:score_type_eq] = 'VerdictScore' }
+      it { expect(subject.sorted_score.last).to eq(verdict_score) }
+    end
+
+    context 'search judge_id' do
+      context 'id exist' do
+        before { params[:judge_id_eq] = schedule_score.judge_id }
+        it { expect(subject.sorted_score.last).to eq(schedule_score) }
+      end
+
+      context 'id not_exist' do
+        before { params[:judge_id_eq] = schedule_score.judge_id + 10 }
+        it { expect(subject.sorted_score).to be_empty }
+      end
+    end
+  end
+end
