9a2803d8cfad60f3ccac139507262e46eac35f76
aronyu <h0935674938@hotmail.com>
2017-03-29 18:14:14 +0800

    jude 會議  bug fix

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,6 +2,7 @@ class Api::BaseController < ApplicationController
   include Api::ErrorConcern
   include Api::PaginationConcern
   skip_before_action :verify_authenticity_token
+  skip_before_action :http_auth_for_staging
   before_action :enable_cors
   before_action :set_default_format
   before_action :set_default_language
