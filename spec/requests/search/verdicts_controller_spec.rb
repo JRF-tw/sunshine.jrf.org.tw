90d55e4b6ae3769147a70c7de5519dea6ec4e00c
aronyu <h0935674938@hotmail.com>
2017-03-29 20:05:29 +0800

    fix bug

--- spec/requests/search/verdicts_controller_spec.rb
+++ spec/requests/search/verdicts_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Search::VerdictsController, type: :request do
   include_context 'create_data_for_request'
-  let!(:verdict) { create :verdict }
+  let!(:verdict) { create :verdict, story: story }
 
   describe '#show' do
     let(:url) { URI.encode("/search/#{code}/#{story.identity}/verdict") }
     before { get url }
     it { expect(response).to be_success }
+    it { expect(response.body).to match(verdict.reason) }
   end
 end
