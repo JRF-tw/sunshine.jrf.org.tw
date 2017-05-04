9add9b8ab526a353bef9d62fba4535e715068a63
aronyu <h0935674938@hotmail.com>
2017-05-04 14:14:05 +0800

    加入追蹤計算庭期狀況

--- db/migrate/20170424041514_add_schedules_cont_to_story.rb
+++ db/migrate/20170424041514_add_schedules_cont_to_story.rb
@@ -1,9 +1,15 @@
 class AddSchedulesContToStory < ActiveRecord::Migration
-  def change
+  def up
     add_column :stories, :schedules_count, :integer, default: 0
 
-    Story.find_each(batch_size: 2000) do |s|
-      Story.reset_counters(s.id, :schedules)  
+    total = Story.all.size
+    Story.find_each(batch_size: 2000).with_index do |s ,i|
+      Story.reset_counters(s.id, :schedules)
+      puts "#{i} / #{total}" if  i % 10000 == 0
     end
+  end
+
+  def down
+    remove_column :stories, :schedules_count
   end
 end
